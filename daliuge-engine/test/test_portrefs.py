1c22e3c56cec993e14f17d3619caa6968f88977c
pritchardn <21726929@student.uwa.edu.au>
2021-11-04 09:49:56 +0800

    Adds a most basic test handle for port reference testing.

--- daliuge-engine/test/test_portrefs.py
+++ daliuge-engine/test/test_portrefs.py
@@ -4,14 +4,13 @@ import unittest
 from dlg import droputils
 from dlg.apps.dynlib import DynlibApp
 from dlg.ddap_protocol import DROPStates
-from dlg.drop import FileDROP
+from dlg.drop import InMemoryDROP
 from test.apps.setp_up import build_shared_library
 
-_libname = "dynlib_example"
-_libfname = "libdynlib_example.so"
+_libname = "dynlib_named_example"
+_libfname = "libdynlib_named_example.so"
 _libpath = os.path.join(os.path.dirname(__file__), 'apps', _libfname)
-print(_libpath)
-_cfgpath = "/tmp/.dlg/testdata/test_basic_imaging.in"
+_cfgpath = os.path.join(os.path.dirname(__file__), 'apps', 'data', 'test.in')
 _normalpath = "/tmp/.dlg/testdata/normal.out"
 
 
@@ -19,16 +18,15 @@ _normalpath = "/tmp/.dlg/testdata/normal.out"
                      "Example dynamic library not available")
 class PortReferenceDynlibTests(unittest.TestCase):
 
-    def test_index_dynlib(self):
-        c = FileDROP('c', 'c', filepath=_cfgpath)  # , nm="Config")
-        n = FileDROP('n', 'n', filepath=_normalpath, nm='Normal')
-        a = DynlibApp('a', 'a', name="CalcNE", lib=_libpath)
-        a.addInput(c)
-        a.addOutput(n)
-        with droputils.DROPWaiterCtx(self, (c, n, a), 5):
-            try:
-                c.setCompleted()
-            except TypeError:  # Just a place-holder to demonstrate the structure of these tests.
-                self.fail("Dynlib failed")
-        self.assertEqual(DROPStates.COMPLETED, n.status)
-        self.assertEqual(droputils.allDropContents(c), droputils.allDropContents(n))
+    def test_named(self):
+        a = InMemoryDROP('a', 'a', filepath=_cfgpath, nm="named_input")
+        c = InMemoryDROP('c', 'c', filepath=_normalpath, nm='named_output')
+        b = DynlibApp('b', 'b', name="CalcNE", lib=_libpath)
+        b.addInput(a)
+        b.addOutput(c)
+        with droputils.DROPWaiterCtx(self, (a, b, c), 10):
+            a.write(os.urandom(32))
+            a.setCompleted()
+        self.assertEqual(DROPStates.COMPLETED, c.status)
+        self.assertEqual(droputils.allDropContents(a), droputils.allDropContents(c))
+
